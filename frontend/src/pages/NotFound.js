b61d4ebbf9b70b480e07851c1108ce0b53c06bfd
vard05 <a.a.arkhipov05@gmail.com>
2024-04-25 17:29:42 +0300

    Update NotFound page text to "Home" for the clickable div.

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 const NotFound = () => {
     const n = useNavigate();
     return (
-        <div onClick={() => n('/')}>123</div>
+        <div onClick={() => n('/')}>Home</div>
     );
 };
 export default NotFound;
